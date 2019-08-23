0e2bb28c60c44bb16249f28466777325b0a1ff84
Yarn <ztytotoro@users.noreply.github.com>
2019-08-23 17:48:54 +0800

    Update fuzzyWord.spec.ts

--- src/patterns/tests/fuzzyWord.spec.ts
+++ src/patterns/tests/fuzzyWord.spec.ts
@@ -1,38 +1,52 @@
 import { FuzzyWord } from '../fuzzyWord';
 import { traverse } from 'src/utils/traverse';
+import { ICharacterSet } from '../base';
 
-test('Word', async () => {
-  const testStr = 'class a extends { }, class';
-  const word = new FuzzyWord({
-    include: null,
-    exclude: [' ', ','],
-    lengthRange: {
-      least: 1,
-      most: Infinity
-    }
+const testFactory = (option: ICharacterSet, title = 'test') => (
+  testStr: any,
+  expectedResult: any
+) => {
+  test(title, async () => {
+    const word = new FuzzyWord(option);
+    const result: string[] = [];
+    word.result.subscribe(t => {
+      result.push(t.content(testStr) as string);
+    });
+    traverse(testStr, word.next.bind(word));
+    expect(result).toEqual(expectedResult);
   });
-  const result: string[] = [];
-  word.result.subscribe(t => {
-    result.push(t.content(testStr) as string);
-  });
-  traverse(testStr, word.next.bind(word));
-  expect(result).toEqual(['class', 'a', 'extends', '{', '}', 'class']);
-});
+};
 
-test('Word', async () => {
-  const testStr = 'class a extends { }, class';
-  const word = new FuzzyWord({
+const test1 = testFactory(
+  {
     include: null,
     exclude: [' ', ','],
     lengthRange: {
       least: 2,
       most: Infinity
     }
-  });
-  const result: string[] = [];
-  word.result.subscribe(t => {
-    result.push(t.content(testStr) as string);
-  });
-  traverse(testStr, word.next.bind(word));
-  expect(result).toEqual(['class', 'extends', 'class']);
-});
+  },
+  'exclude space and comma, at least 2 character'
+);
+
+const test2 = testFactory(
+  {
+    include: null,
+    exclude: [' ', ','],
+    lengthRange: {
+      least: 1,
+      most: Infinity
+    }
+  },
+  'exclude space and comma, at least 1 character'
+);
+
+test1('class a extends { }, class', ['class', 'extends', 'class']);
+test2('class a extends { }, class', [
+  'class',
+  'a',
+  'extends',
+  '{',
+  '}',
+  'class'
+]);
